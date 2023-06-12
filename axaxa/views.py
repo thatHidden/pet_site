f9c13bcc9b03873e7febc6444f3c4df04c79dfb9
thatHidden <blond2stone@gmail.com>
2023-06-12 17:26:54 +0700

    Start2

--- axaxa/views.py
+++ axaxa/views.py
@@ -89,7 +89,6 @@ class HomeView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['cunts'] = Cars.objects.filter(is_active=True).count()  # TODO СРОЧНО ИСПРАВИТЬ ОШИБКУ В НАЗВАНИИ
         context['available'] = Cars.objects.values_list('brand', flat=True).distinct()  # distinct() - возвращает все значения поля без повторов
         return context
 
